6ee14fb62cdf23596b77a71d86b509a4338b1ae2
Amisha <21j03.amisha@sjec.ac.in>
2023-09-13 20:30:29 +0530

    error corrected

--- CopyPassword.py
+++ CopyPassword.py
@@ -1,13 +1,18 @@
+#Write a program that takes the account name for instance email or blog from command line arguments and copies the acounts password to the clipboard so that the user can paste it into a password field
+#works in vs code
 import sys
 import pyperclip
-PASSWORDS={'email':'myemailpassword','blog':'myblogpassword','social':'mysocialmediapassword'}
+PASSWORDS={
+    'email':'myemailpassword',
+    'blog':'myblogpassword',
+    'social':'mysocialpassword'
+}
 if len(sys.argv)<2:
-  print('Usage:python password_manager.py[account]-copy account password')
+  print('Usage:python password_manager.py[account]-copy accountpassword')
   sys.exit()
-account=sys.argv[1]
+account =sys.argv[1]
 if account in PASSWORDS:
   pyperclip.copy(PASSWORDS[account])
-  print('Password for'+account+'copied to clipboard.')
+  print('Password for '+account+' copied to clipboard.')
 else:
-  print('There is no account named'+account)
-  
+  print('There is no account named '+account)
