5efbc297baaebd7e68275362b01760f3583a2317
Amisha <21j03.amisha@sjec.ac.in>
2023-09-24 23:54:55 +0530

    code added

--- lab9.py
+++ lab9.py
@@ -1,2 +1,10 @@
 import os
-if not os.path.exists("lab9")
+if not os.path.exists("lab9"):
+    os.mkdir("lab9")
+with open("lab9/file1.txt","w") as file1:
+    file1.write("VTU")
+with open("lab9/file2.txt","w") as file2:
+    file2.write("UNIVERSITY")
+with open("lab9/file1.txt","r") as file1, open("lab9/file2.txt","r") as file2, open("lab9/file3.txt","w") as file3:
+    content=file1.read()+file2.read()
+    file3.write(content)
