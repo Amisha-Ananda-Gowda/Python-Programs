842bf73096308d686b5a8d824213e7c932580d27
Amisha <21j03.amisha@sjec.ac.in>
2023-09-24 22:33:38 +0530

    code added

---
+++ Sum&Product.py
@@ -0,0 +1,14 @@
+n=int(input("Enter the number of elements:"))
+num=[]
+print("Enter ",n," Elements:")
+for i in range(n):
+    num.append(int(input()))
+sum=0
+product=1
+for i in range(n):
+    if(num[i]%2==0):
+        sum+=num[i]
+    else:
+        product*=num[i]
+print("Sum of even numbers= ",sum)
+print("Product of odd numbers = ",product)
