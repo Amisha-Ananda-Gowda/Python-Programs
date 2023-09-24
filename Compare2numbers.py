028c413973647a4bbbd6130e43090c5936dd17c9
Amisha <21j03.amisha@sjec.ac.in>
2023-09-24 21:43:50 +0530

    code added

---
+++ Compare2numbers.py
@@ -0,0 +1,9 @@
+print("Enter two numbers:")
+a=int(input())
+b=int(input())
+if(a>b):
+    print("{0} is greater than {1}".format(a,b))
+elif(a<b):
+    print("{0} is lesser than {1}".format(a,b))   
+else:
+    print("{0} is equal to {1}".format(a,b))  
