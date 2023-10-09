588e654e98fec86d756f37037c0ed23dbce6b9ce
Amisha <21j03.amisha@sjec.ac.in>
2023-10-09 16:57:23 +0530

    modified code

--- sentence.py
+++ sentence.py
@@ -4,6 +4,7 @@ upper=0
 lower=0
 word=sentence.split()
 words=len(word)
+
 for i in sentence:
     if i.isdigit():
         digit+=1
@@ -11,7 +12,7 @@ for i in sentence:
         upper+=1
     elif i.islower():
         lower+=1
-        
+
 print("Number of words : ",words)
 print("Number of digits : ",digit)
 print("Number of uppercase letters : ",upper)
